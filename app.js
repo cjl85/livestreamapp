3bddbd1356009336d07076f81481d64b04e68ad3
CL <cl@CLs-MacBook-Pro.local>
2019-10-26 16:30:25 -0600

    add1

--- app.js
+++ app.js
@@ -64,13 +64,8 @@ app.service('ideas').create({
   viewer: 'John Doe'
 });
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+app.use(express.static('build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
+});
