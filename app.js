5678016047b742047278acd750c9b172482da1e2
CL <cl@CLs-MacBook-Pro.local>
2019-10-26 18:42:28 -0600

    add10

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.service('ideas').create({
   viewer: 'John Doe'
 });
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/build/index.html'));
 });
